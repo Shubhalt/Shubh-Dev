f5b09a1af73db95988ef1b05eb10748d56182211
shubhranshalt@gmail.com <shubhranshalt@gmail.com>
2023-05-19 04:54:05 +0000

    commit-"8-Incremental+ORDER upd"

--- models/customer/orders.sql
+++ models/customer/orders.sql
@@ -1,12 +1,17 @@
-{{ config(materialized='table',
-          partition_by={
-            "field":"order_date"}) }}
+{{
+    config(
+        materialized='incremental',
+        unique='dbt_utils.surrogate_key(order_id, customer_id)',
+        partition_by={
+            "field":"order_date"}
+    )
+}}
 
-with source_data as (
-    select '1' as order_id, cust_id as customer_id,
-    'car' as order_name, current_date as order_date
-    from Shubh_Test.customer
-)
-
-select *
-from source_data
+select ROW_NUMBER() OVER (ORDER BY cust_id) + 1000 as order_id, 
+cust_id as customer_id,
+CASE
+    WHEN rand() < 1.0/3 THEN 'Laptop'
+    WHEN rand() < 2.0/3 THEN 'Card'
+    ELSE 'Chip' end as order_name, 
+current_date as order_date
+from Shubh_Test.customer
